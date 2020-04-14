30401c3d6161fa69289acd97be25c42f38df6b81
ADiks <antondikson@gmail.com>
2020-04-14 12:07:19 +0300

    Updates

---
+++ precache-manifest.b7cf3acacb4d9bbc182891cd89f4ed57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b597d9cc180e53869e24",
+    "url": "/movie-search/static/js/main.e11d9e22.chunk.js"
+  },
+  {
+    "revision": "467f242a197283b43b88",
+    "url": "/movie-search/static/js/runtime~main.5125b6b9.js"
+  },
+  {
+    "revision": "7ce34ebf2a5cbb4566d2",
+    "url": "/movie-search/static/css/2.2cd4e56f.chunk.css"
+  },
+  {
+    "revision": "7ce34ebf2a5cbb4566d2",
+    "url": "/movie-search/static/js/2.1c8d0edb.chunk.js"
+  },
+  {
+    "revision": "327f1aa70a739b120be3efc0af3dd910",
+    "url": "/movie-search/index.html"
+  }
+];
